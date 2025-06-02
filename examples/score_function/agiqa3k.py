2f8b23ad6fefd1b81204d6a96fc386d004461128
Coobiw <brian.bw.qu@gmail.com>
2025-06-03 04:04:31 +0800

    support continuous l1 reward

--- examples/score_function/agiqa3k.py
+++ examples/score_function/agiqa3k.py
@@ -5,6 +5,15 @@ def grade_answer(pred, gt, threshold):
     # print("threshold:\t", threshold)
     return (abs(float(pred) - float(gt)) < threshold)
 
+def grade_answer_continuous(pred, gt, threshold):
+    diff = abs(float(pred) - float(gt))
+
+    if diff < threshold:
+        return 1 - diff
+    else:
+        return 0.
+            
+
 
 def format_reward(predict_str: str) -> float:
     pattern = re.compile(r"<think>.*?</think>\s*<answer>.*?</answer>", re.DOTALL)
@@ -27,10 +36,32 @@ def accuracy_reward(predict_str: str, ground_truth: str, threshold: float) -> fl
 
     return 0.0
 
+def accuracy_reward_continuous(predict_str: str, ground_truth: str, threshold: float) -> float:
+    try:
+        content_match = re.search(r"<answer>(.*?)</answer>", predict_str)
+        given_answer = content_match.group(1).strip() if content_match else predict_str.strip()
+        if isinstance(ground_truth, float) or isinstance(ground_truth, int):
+            ground_truth = str(ground_truth)
+        return grade_answer_continuous(given_answer, ground_truth.strip(), threshold)
+
+    except Exception as e:
+        # print(e)
+        pass
+
+    return 0.0
 
 def compute_score(predict_str: str, ground_truth: str, format_weight: float = 0.5, threshold: float = 0.35) -> Dict[str, float]:
     format_score = format_reward(predict_str)
     accuracy_score = accuracy_reward(predict_str, ground_truth, threshold)
+    return {
+        "overall": (1 - format_weight) * accuracy_score + format_weight * format_score,
+        "format": format_score,
+        "accuracy": accuracy_score,
+    }
+
+def compute_score_continuous(predict_str: str, ground_truth: str, format_weight: float = 0.5, threshold: float = 0.35) -> Dict[str, float]:
+    format_score = format_reward(predict_str)
+    accuracy_score = accuracy_reward_continuous(predict_str, ground_truth, threshold)
     return {
         "overall": (1 - format_weight) * accuracy_score + format_weight * format_score,
         "format": format_score,
